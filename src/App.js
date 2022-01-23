f792331ddb0af9378e439aa94bd5112fd784d534
Ross Shelford <R.Shelford@hotmail.co.uk>
2022-01-23 09:11:19 +0000

    Initial setup. Added Favicon

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Hello world</h1>
+    </React.Fragment>
+
+  );
+}
+
+export default App;
