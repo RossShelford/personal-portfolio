07808cb5a617b89264509a5a3259205f1831d520
Ross Shelford <R.Shelford@hotmail.co.uk>
2022-01-27 12:17:33 +0000

    Added React Router to NavBar.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import HeroText from './components/HeroText/HeroText';
+import { BrowserRouter as Router, Routes as Switch, Route} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="w-auto h-auto flex-col-reverse dark:bg-charcoal transition-all dark:transition-all">
-      <NavBar />
-      <HeroText text="HELLO WORLD"/>
-    </div>
+    <Router>
+      <div className="w-auto h-auto flex-col-reverse dark:bg-charcoal transition-all dark:transition-all">
+        <NavBar />
+        <Switch>
+          <Route path="/" exact>
+            
+          </Route>
+        </Switch>
+        <HeroText text="HELLO WORLD"/>
+      </div>
+    </Router>
   );
 }
 
