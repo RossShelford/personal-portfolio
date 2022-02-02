221daee6e3d64f4c3677579c6345828136b024a8
Ross Shelford <rossob@live.co.uk>
2022-02-02 23:57:33 +0000

    Added ButtonFilterBar hook working, Currently unfinished.

--- src/components/WorkPage/ButtonFilterBar.jsx
+++ src/components/WorkPage/ButtonFilterBar.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-
-
-
+import React, { useState } from 'react';
 
 function ButtonFilterBar() {
+
+  const [activeFilter, setFilter] = useState("All")
+
   return (
     <div className="h-auto">
         <div className="bg-gray-50 dark:bg-[#292929] transition-all h-16 flex items-center justify-center">
-            <button className="inline-flex h-10 m-5 p-2 px-3 text-sm items-center justify-center text-center font-poppins select-none rounded-xl min-w-[3rem] text-stone-800 bg-neutral-300 transition-all dark:bg-neutral-500 dark:text-stone-300  ">
-                All</button>
-            <button className="inline-flex h-10 m-3 p-2 text-sm items-center justify-center text-center font-poppins select-none text-stone-500 dark:text-stone-300 cursor-pointer">
-                Design</button>
-            <button className="inline-flex h-10 m-3 p-2 text-sm items-center justify-center text-center font-poppins select-none text-stone-500 dark:text-stone-300 cursor-pointer">
-                Development</button>
+            <button onClick={()=> setFilter("All")} className="inline-flex h-10 m-5 p-2 px-3 text-sm items-center justify-center text-center font-poppins select-none rounded-xl min-w-[3rem] text-stone-800 bg-neutral-300 transition-all dark:bg-neutral-500 dark:text-stone-300">
+                All
+            </button>
+            <button onClick={()=> setFilter("Design")} className="inline-flex h-10 m-3 p-2 text-sm items-center justify-center text-center font-poppins select-none text-stone-500 dark:text-stone-300 cursor-pointer">
+                Design
+            </button>
+            <button onClick={()=> setFilter("Development")} className="inline-flex h-10 m-3 p-2 text-sm items-center justify-center text-center font-poppins select-none text-stone-500 dark:text-stone-300 cursor-pointer">
+                Development
+            </button>
+            <button onClick={()=> console.log(activeFilter)}>
+              LogActiveFilter
+            </button>
         </div>
     </div>
   );
 }
 
 export default ButtonFilterBar;
-
-
-
-
