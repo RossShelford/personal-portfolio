30da48a893b0486b478b81335cd951520cafe9dd
Ross Shelford <rossob@live.co.uk>
2022-02-03 21:40:02 +0000

    Made ButtonFilterBar hook state global into WorkPageContainer.

--- src/components/WorkPage/ButtonFilterBar.jsx
+++ src/components/WorkPage/ButtonFilterBar.jsx
@@ -1,19 +1,15 @@
-import React, { useState } from 'react';
-
-function ButtonFilterBar() {
-
-  const [activeFilter, setFilter] = useState("All")
+function ButtonFilterBar({activeFilter, handleSetFilter }) {
 
   return (
     <div className="h-auto">
         <div className="bg-gray-50 dark:bg-[#292929] transition-all h-16 flex items-center justify-center">
-            <button onClick={()=> setFilter("All")} className="inline-flex h-10 m-5 p-2 px-3 text-sm items-center justify-center text-center font-poppins select-none rounded-xl min-w-[3rem] text-stone-800 bg-neutral-300 transition-all dark:bg-neutral-500 dark:text-stone-300">
+            <button onClick={() => handleSetFilter("All")} className="inline-flex h-10 m-5 p-2 px-3 text-sm items-center justify-center text-center font-poppins select-none rounded-xl min-w-[3rem] text-stone-800 bg-neutral-300 transition-all dark:bg-neutral-500 dark:text-stone-300">
                 All
             </button>
-            <button onClick={()=> setFilter("Design")} className="inline-flex h-10 m-3 p-2 text-sm items-center justify-center text-center font-poppins select-none text-stone-500 dark:text-stone-300 cursor-pointer">
+            <button onClick={() => handleSetFilter("Design")} className="inline-flex h-10 m-3 p-2 text-sm items-center justify-center text-center font-poppins select-none text-stone-500 dark:text-stone-300 cursor-pointer">
                 Design
             </button>
-            <button onClick={()=> setFilter("Development")} className="inline-flex h-10 m-3 p-2 text-sm items-center justify-center text-center font-poppins select-none text-stone-500 dark:text-stone-300 cursor-pointer">
+            <button onClick={() => handleSetFilter("Development")} className="inline-flex h-10 m-3 p-2 text-sm items-center justify-center text-center font-poppins select-none text-stone-500 dark:text-stone-300 cursor-pointer">
                 Development
             </button>
             <button onClick={()=> console.log(activeFilter)}>
--- src/components/WorkPage/WorkPageContainer.jsx
+++ src/components/WorkPage/WorkPageContainer.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import ButtonFilterBar from "./ButtonFilterBar.jsx";
 import WorkItems from "./WorkItems.jsx";
 
 function WorkPage() {
 
+  const [activeFilter, setFilter] = useState("All");
+
+  function handleSetFilter(arg) {
+    const setActiveFilter = arg
+    setFilter(setActiveFilter)
+  }
+
   return (
   <>
-    <ButtonFilterBar />
+    <ButtonFilterBar activeFilter={activeFilter}  handleSetFilter={handleSetFilter}/>
     <WorkItems />
   </>
   );
