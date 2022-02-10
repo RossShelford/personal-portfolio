8a24281bacd61902126b04245cd5e8c4a2804543
Ross Shelford <rossob@live.co.uk>
2022-02-10 20:36:57 +0000

    Adjusted sizing fitler bar.

--- src/components/WorkPage/ButtonFilterBar.jsx
+++ src/components/WorkPage/ButtonFilterBar.jsx
@@ -9,18 +9,18 @@ function ButtonFilterBar({activeFilter, handleSetFilter }) {
   return (
     <div className="h-auto">
         <div className="bg-gray-50 dark:bg-[#292929] transition-all h-16 flex items-center justify-center">
-            <h3 className="font-poppins text-xs uppercase text-gray-400 dark:text-zinc-500 select-none">Work Type:</h3>
-            <button onClick={() => handleSetFilter("All")} className={`${handleIsActiveFilter("All")} text-flint inline-flex h-10 m-5 p-2 px-3 text-sm items-center justify-center text-center font-poppins select-none transition-all`}>
+            <h3 className="font-poppins text-xs uppercase text-gray-400 dark:text-zinc-500 select-none pl-5 p-2">Work Type:</h3>
+            <button onClick={() => handleSetFilter("All")} className={`${handleIsActiveFilter("All")} text-flint inline-flex h-10 sm:m-5 m-2 p-2 px-3 text-sm items-center justify-center text-center font-poppins select-none transition-all`}>
                 All
             </button>
 
             
-            <button onClick={() => handleSetFilter("Design")} className={`${handleIsActiveFilter("Design")} text-flint inline-flex h-10 m-3 p-2 text-sm items-center justify-center text-center font-poppins select-none cursor-pointer transition-all`}>
+            <button onClick={() => handleSetFilter("Design")} className={`${handleIsActiveFilter("Design")} text-flint inline-flex h-10 sm:m-3 m-1 p-2 text-sm items-center justify-center text-center font-poppins select-none cursor-pointer transition-all`}>
                 Design
             </button>
 
 
-            <button onClick={() => handleSetFilter("Development")} className={`${handleIsActiveFilter("Development")} text-flint inline-flex h-10 m-3 p-2 text-sm items-center justify-center text-center font-poppins select-none cursor-pointer transition-all`}>
+            <button onClick={() => handleSetFilter("Development")} className={`${handleIsActiveFilter("Development")} text-flint inline-flex h-10 sm:m-3 m-1 mr-5 p-2 text-sm items-center justify-center text-center font-poppins select-none cursor-pointer transition-all`}>
                 Development
             </button>
         </div>
