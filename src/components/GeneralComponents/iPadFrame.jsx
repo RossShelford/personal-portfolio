c7a33c4000c84ebe56df47f6e8808ab1a124da8d
Ross Shelford <rossob@live.co.uk>
2022-02-23 19:47:34 +0000

    Added props to iPad component.

--- src/components/GeneralComponents/iPadFrame.jsx
+++ src/components/GeneralComponents/iPadFrame.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import BaileyWebVid from '../WorkPage/NFT/assets/Timelapse_Bailey.webm'
 import ipad from '../GeneralComponents/assets/ipad-frame.png'
 
-function IPadFrame() {
+function IPadFrame(props) {
   return (
-    <div className='m-10 relative flex justify-center items-center'>
+    <div className='m-0 relative flex justify-center items-center h-[40rem] w-[40rem]'>
       <img className='absolute z-20' src={ipad} alt='iPad'/>
-      <video className='absolute z-10 scale-[0.93]' src={BaileyWebVid} loop="loop" autoplay="autoplay"></video>
+      <video className='absolute z-10 scale-[0.94]' src={props.content} loop="loop" autoplay="autoplay"></video>
     </div>
   )
 }
--- src/components/WorkPage/NFT/NFTContainer.jsx
+++ src/components/WorkPage/NFT/NFTContainer.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import IPadFrame from '../../GeneralComponents/IPadFrame';
+import BaileyWebVid from './assets/Timelapse_Bailey.webm';
 
 function NFTContainer() {
   return (
-    <div className='flex flex-col justify-center'>
-      <IPadFrame/>
-      <p>
-        Hello 123
-      </p>
+    <div className='flex flex-col justify-center items-center'>
+      
+      <div className='selection:bg-salmon selection:text-flint max-w-screen-lg'>
+        <h3 className='font-poppins text-3xl md:px-32 px-20 pb-3 pt-20 dark:text-cream'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente</h3>
+        <p className='font-poppins text-sm md:px-32 px-20 leading-relaxed dark:text-cream'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, minima quidem vitae at repellendus sint nesciunt nemo optio ipsam, esse ut maxime consectetur adipisci dicta vel libero error nobis mollitia unde eos. Doloribus id nesciunt veritatis eius nisi sit veniam a omnis blanditiis eum, vel quam ad natus adipisci ex consequatur non corporis iste inventore optio. Quidem totam quos veniam minus iste maiores provident sapiente nihil amet! Voluptatum impedit totam error, non, voluptatem ipsa a beatae sed illo itaque corrupti dignissimos? Quasi itaque expedita qui dolorem earum. Libero vel reprehenderit voluptates qui, placeat similique delectus nisi in officiis, sint provident.</p>
+      </div>
+      <IPadFrame content={BaileyWebVid}/>
+
     </div>
 
 )
